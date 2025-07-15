d6cb89acff2628d789840852caac52d4d83f3997
vedhavk <vedhavk2004@gmail.com>
2025-07-15 22:55:01 +0530

    Re-added client folder as normal directory

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import StartStory from "./components/StartStory";
+
+function App() {
+  return (
+    <div className="w-full">
+      <StartStory />
+    </div>
+  );
+}
+
+export default App;
